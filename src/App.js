b49c31516799925e8aef273a860f89f6912a5e31
Ihab Hassoun <ihab_hassoun1@hotmail.com>
2025-11-18 18:58:30 +0200

    add a simple React component to display a welcome message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <h1>Hello, React!</h1>
+    <p>Welcome to your first React application.</p>
+  </div>;
 }
 
 export default App;
